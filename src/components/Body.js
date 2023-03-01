3c45c1ddc4b7ff48d9b492fa7cbcec52834cfad7
unknown <mdsakiluzzaman@gmail.com>
2023-03-01 23:44:47 +0600

    featured sorting working

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,63 +1,64 @@
-import React, { useEffect } from 'react';
-import Navbar from './Navbar'
-import BookCard from './BookCard';
-import Form from './Form'
-import { useDispatch, useSelector } from 'react-redux';
-import fetchBooks from '../redux/books/thunk/fetchBooks';
-
-
+import React, { useEffect, useState } from "react";
+import Navbar from "./Navbar";
+import BookCard from "./BookCard";
+import Form from "./Form";
+import { useDispatch, useSelector } from "react-redux";
+import fetchBooks from "../redux/books/thunk/fetchBooks";
 
 function Body() {
-    const books = useSelector(state=>state.books)
-    const dispatch = useDispatch()
-    useEffect(() => {
-      
-    dispatch(fetchBooks)
-   
-      return () => {
-        
-      }
-    }, [dispatch])
+  const books = useSelector((state) => state.books);
+  const dispatch = useDispatch();
+  const [catagory,setCatagory] = useState('all');
+  let filteredBooks=[];
+  useEffect(() => {
+    dispatch(fetchBooks);
 
-    console.log(books);
-    
-    
+    return () => {};
+  }, [dispatch]);
+  if(catagory=='featured'){
+    filteredBooks = [...books.filter(item=>item.featured==true)]
+  }
+  else{
+    filteredBooks = [...books];
+  }
+  console.log(books);
+ 
   return (
     <div>
-      <Navbar/>
-        <main class="py-12 2xl:px-6">
-          <div class="container grid xl:grid-cols-[auto_350px] 2xl:grid-cols-[auto_400px] gap-4 2xl:gap-8">
-            <div class="order-2 xl:-order-1">
-              <div class="flex items-center justify-between mb-12">
-                <h4 class="mt-2 text-xl font-bold">Book List</h4>
+      <Navbar />
+      <main class="py-12 2xl:px-6">
+        <div class="container grid xl:grid-cols-[auto_350px] 2xl:grid-cols-[auto_400px] gap-4 2xl:gap-8">
+          <div class="order-2 xl:-order-1">
+            <div class="flex items-center justify-between mb-12">
+              <h4 class="mt-2 text-xl font-bold">Book List</h4>
 
-                <div class="flex items-center space-x-4">
-                  <button class="filter-btn active-filter" id="lws-filterAll">
-                    All
-                  </button>
-                  <button class="filter-btn" id="lws-filterFeatured">
-                    Featured
-                  </button>
-                </div>
-              </div>
-              <div class="lws-bookContainer">
-                {/* <!-- Card 1 --> */}
-                {
-                    books.map(item=>{
-                        return (<BookCard bookInfo={item}/>)
-                    })
-                }
-                
+              <div class="flex items-center space-x-4">
+                <button class={`filter-btn ${catagory=="all" && "active-filter"}`} id="lws-filterAll" onClick={()=>setCatagory("all")}>
+                  All
+                </button>
+                <button
+                  class={`filter-btn ${catagory=="featured" && "active-filter"}`}
+                  id="lws-filterFeatured"
+                  onClick={()=>setCatagory("featured")}
+                >
+                  Featured
+                </button>
               </div>
             </div>
-            <div>
-            <Form/>
-            
+            <div class="lws-bookContainer">
+              {/* <!-- Card 1 --> */}
+              {filteredBooks.map((item) => {
+                return <BookCard bookInfo={item} />;
+              })}
             </div>
           </div>
-        </main>
+          <div>
+            <Form />
+          </div>
+        </div>
+      </main>
     </div>
-  )
+  );
 }
 
-export default Body
+export default Body;
